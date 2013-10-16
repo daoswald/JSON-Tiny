38c0340144be69d57acc4e9d73afc3a17721aa8b
David Oswald <daoswald@gmail.com>
2013-10-16 09:05:01 -0600

    encode_value now checks $ref =~ /SCALAR/ instead of eq "SCALAR"

--- lib/JSON/Tiny.pm
+++ lib/JSON/Tiny.pm
@@ -309,13 +309,14 @@ sub _encode_value {
     return _encode_object($value) if $ref eq 'HASH';
 
     # True or false
-    return $$value ? 'true' : 'false' if $ref eq 'SCALAR';
+#    return $$value ? 'true' : 'false' if $ref eq 'SCALAR';
     return $value  ? 'true' : 'false' if $ref eq 'JSON::Tiny::_Bool';
 
     # Blessed reference with TO_JSON method
     if (Scalar::Util::blessed $value && (my $sub = $value->can('TO_JSON'))) {
       return _encode_value($value->$sub);
     }
+    return $$value ? 'true' : 'false' if $ref =~ /SCALAR/;
   }
 
   # Null
